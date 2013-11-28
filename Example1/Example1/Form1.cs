bdbd0cfd72714d4b82576dc14154c2af1e0f8651
gmlacrosse <mlacrosse3@gmail.com>
2013-11-28 09:54:04 -0600

    Adding Example1

---
+++ Example1/Example1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+// 
+// Author Guy Matthew LaCrosse
+// 
+// This is just a simmple forms example, the C# equivalent of Hello World.
+//
+// Components:
+//      Form
+//      Label
+//      Button
+//
+// If you have Visual Studio just create a New Project of type C#, Windows, Windows Forms Application
+// drag and drop the label, button, copy the button. Double click the button to create an event
+// handler for changing the Text of label1.
+
+
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = " ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = " ";
+        }
+    }
+}
